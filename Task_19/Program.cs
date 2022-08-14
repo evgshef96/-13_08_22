0b84e66c4655d895ea6fd76742977704bab906f7
Evgeny <jeka-291@mail.ru>
2022-08-14 16:43:00 +0300

    Домашнее задание к семинару 13.08

---
+++ Task_19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+
+void CheckPalindromicNumber(int number)
+{
+    if(number >= 10000)
+    {
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
+
+            if(division1 == remainder1)
+            {
+                number = number / 10;
+                int remainder2 = (number / 100) % 10;
+                int remainder3 = number % 10;
+                if(remainder2 == remainder3)
+                    Console.WriteLine("Да");
+            }
+            else 
+            Console.WriteLine("Нет");
+            
+    }
+    else
+    Console.WriteLine("Некорректное число!");
+}
+
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+CheckPalindromicNumber(number);
---
+++ Task_21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+double DistanceBetweenCoordinates (double xa, double ya, double za, double xb, double yb, double zb)
+{
+    return Math.Sqrt(Math.Pow(xb-xa,2) + Math.Pow(yb-ya,2) + Math.Pow(zb-za,2));
+}
+
+Console.WriteLine("Введите значение координаты по оси x точки а: ");
+double xa = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение координаты по оси y точки а: ");
+double ya = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение координаты по оси z точки a: ");
+double za = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение координаты по оси x точки b: ");
+double xb = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение координаты по оси y точки b: ");
+double yb = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение координаты по оси z точки b: ");
+double zb = double.Parse(Console.ReadLine()!);
+
+double distance = DistanceBetweenCoordinates(xa,ya,za,xb,yb,zb);
+Console.WriteLine($"{distance:f2}");
